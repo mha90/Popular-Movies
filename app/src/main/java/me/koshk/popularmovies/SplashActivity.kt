5cdfd59328a00994a51fc4051900e1cc03801ae2
mha90 <mhabulazm@gmail.com>
2018-05-13 17:43:05 +0200

    Init Commit

---
+++ app/src/main/java/me/koshk/popularmovies/SplashActivity.kt
@@ -0,0 +1,12 @@
+package me.koshk.popularmovies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
